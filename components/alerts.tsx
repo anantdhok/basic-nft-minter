e358c1fcfe43a2fa971838477ba421f2e5580dcb
anantdhok <anantdhok16@gmail.com>
2023-03-12 20:59:24 +0530

    Add classnames to alert types

--- components/alerts.tsx
+++ components/alerts.tsx
@@ -6,16 +6,27 @@ interface AlertsProps {
 }
 
 const alertIcons = {
-  info: "M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z",
-  success: "M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z",
-  warning:
-    "M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z",
-  error: "M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+  info: {
+    class: "alert-info",
+    icon: "M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+  },
+  success: {
+    class: "alert-success",
+    icon: "M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+  },
+  warning: {
+    class: "alert-warning",
+    icon: "M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+  },
+  error: {
+    class: "alert-error",
+    icon: "M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+  }
 };
 
-function Alerts({ type, message }: AlertsProps) {
+function Alerts({ type = "info", message }: AlertsProps) {
   return (
-    <div className={`alert alert-${type} shadow-lg`} id="status">
+    <div className={`alert ${alertIcons[type].class} shadow-lg`} id="status">
       <div>
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -23,7 +34,7 @@ function Alerts({ type, message }: AlertsProps) {
           fill="none"
           viewBox="0 0 24 24"
         >
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={alertIcons[type || 0]} />
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={alertIcons[type].icon} />
         </svg>
         <span>{message}</span>
       </div>
